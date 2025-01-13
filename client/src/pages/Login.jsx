088e4d411c1e4975e2143353c2097e930d876558
yk-tuturu <waikei.koh@gmail.com>
2025-01-14 01:51:01 +0800

    login/register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,42 @@
-import {useState} from "react"
+import {useState, useContext} from "react"
+import axios from "axios"
 
 const Login = () => {
+    const [error, setError] = useState("")
+    const [inputs, setInputs] = useState({
+        email: "",
+        password: ""
+    })
+
+    function handleChange(e) {
+        setInputs((prev) => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            
+        } catch (err) {
+            console.log(err)
+            setError(err);
+        }
+    }
     return (
         <div className="login-container">
             <div className="login-inner">
-
+                <div className="login-header">
+                    <h1>Login</h1>
+                </div>
+                <div className="login-content">   
+                    <input required type="text" placeholder="email" name="email" onChange={handleChange}></input>
+                    <input required type="password" placeholder="password" name="password" onChange={handleChange}></input>
+                </div>
+                <div className="footer">
+                    <p className="error">{error} </p>
+                    <button onClick={handleSubmit}>Login</button>
+                    <p>Don't have an account? <a href="/register"><u>Register</u></a> here</p>
+                </div>
+                
             </div>
         </div>
         

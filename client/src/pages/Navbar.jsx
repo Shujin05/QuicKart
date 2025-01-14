9d3c95d5cf5ff10cc3c157b32ae9fb25ea394988
yk-tuturu <waikei.koh@gmail.com>
2025-01-14 09:26:07 +0800

    navbar if condition

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -4,13 +4,26 @@ import {useLocation} from "react-router-dom"
 
 
 const Navbar = () => {
+    const [isAdmin, setIsAdmin] = useState(false)
+
     return (
         <div className="sidebar">
             <h1 className="sidebar-title">Website</h1>
-            <a href="/">Home</a>
-            <a href="/products">Products</a>
-            <a href="/transaction">Transaction History</a>
-            <a href="/transaction">Logout</a>
+            {
+                isAdmin ? <>
+                    <a href="/">Home</a>
+                    <a href="/products">Manage Users</a>
+                    <a href="/transaction">Inventory</a>
+                    <a href="/transaction">Logout</a>
+                </> :
+                <>
+                    <a href="/">Home</a>
+                    <a href="/products">Products</a>
+                    <a href="/transaction">Transaction History</a>
+                    <a href="/transaction">Logout</a>
+                </>
+            }
+            
         </div>
     )
 }

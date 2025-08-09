42fa45cb37cfbe0d51b118b497972d729037d486
shu jin <limshujin2005@gmail.com>
2025-08-09 14:51:09 +0800

    updated backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"; 
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // connect to mongoDB database 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://limshujin2005:hackforgood2025@cluster0.3pydb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=> console.log("DB connected"))
+    await mongoose.connect(process.env.MONGODB_URI).then(()=> console.log("DB connected"))
 } 
